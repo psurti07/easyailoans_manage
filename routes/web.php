e704690faba9bc436529f1d7ae41a9420a62e83e
vw-uday <verloop.dev4@gmail.com>
2026-05-30 18:28:21 +0530

    sync issue solve

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\InvoiceSyncController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Partnersturnover;
 
@@ -8,3 +9,5 @@ Route::get('error',function(){ return view('error'); });
 /*Route::get('/selfapplyForJune',[Partnersturnover::class, 'selfapplyForJune']);*/
 Route::get('/selfapplyTurnover',[Partnersturnover::class, 'selfapply']);
 Route::get('/hireagentTurnover',[Partnersturnover::class, 'hireagent']);
+
+Route::post('/sync-invoice-data', [InvoiceSyncController::class, 'syncInvoiceData']);
